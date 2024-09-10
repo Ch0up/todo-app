ebf3a136f8e051161cc03a47ba6da63da61243b9
Ch0up <joris.kosacki@hotmail.fr>
2024-09-10 17:13:39 +0200

    add mdi icon

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
+    <h1>Ma liste de tâches</h1>
+  </div>
+  <div>
+    <v-card>
+      <TodoActions />
     <TodoForm />
     <TodoList />
+    </v-card>
+   
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import TodoForm from './components/TodoForm.vue';
-import TodoList from './components/TodoList.vue';
+import { defineComponent } from "vue";
+import TodoForm from "./components/TodoForm.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoActions from "./components/TodoActions.vue";
 
 export default defineComponent({
   components: {
     TodoForm,
     TodoList,
+    TodoActions,
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 import './style.css'
 
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
